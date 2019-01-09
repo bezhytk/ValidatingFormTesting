227c07138833bffbdbf44c1ad49298928b06c756
Thapanee Kps <thapanee.kps@gmail.com>
2019-01-09 16:34:25 +0700

    editted all test cases

--- Test Suites/All Test Cases.ts
+++ Test Suites/All Test Cases.ts
@@ -11,49 +11,55 @@
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <testSuiteGuid>285d1f40-6ecf-4699-bb64-5af948fd2430</testSuiteGuid>
    <testCaseLink>
-      <guid>9c7f1dc0-c276-47e6-bcf1-1618fcca6994</guid>
+      <guid>958e4bb1-4210-470f-b47b-eb068c5b823a</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Login Age by alphabet</testCaseId>
    </testCaseLink>
    <testCaseLink>
-      <guid>4bf3b63f-26d2-473a-a841-fa4b7ccab03c</guid>
+      <guid>f1b444ab-a20a-45a1-95ab-bb9a0cc14787</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login Age under 18</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>a48d7da2-2164-4ae0-98be-c0cfda2c14ff</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Login Empty Age</testCaseId>
    </testCaseLink>
    <testCaseLink>
-      <guid>c0a07b7f-ccc6-4df6-8a66-e83d4f8ffe96</guid>
+      <guid>7437d230-9a95-440b-aa62-fc0d6b420060</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Login Empty Name and Empty Age</testCaseId>
    </testCaseLink>
    <testCaseLink>
-      <guid>fb72600e-75be-46de-92e6-a75a3ce8d4c8</guid>
+      <guid>ff8dca23-7c64-445b-a816-281fb452d898</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Login Invalid Name and Age by alphabet</testCaseId>
    </testCaseLink>
    <testCaseLink>
-      <guid>10633385-e153-4ab6-849e-b70649fdfbb4</guid>
+      <guid>e984f8a9-6954-4219-9386-c950e01301ac</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Login Invalid Name and Age greater or equal to 18</testCaseId>
    </testCaseLink>
    <testCaseLink>
-      <guid>e15e2a46-e756-4e19-80d0-827639c05667</guid>
+      <guid>cc854527-69a3-496d-89f7-e65b0896fa89</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Login Invalid Name and Age under 18</testCaseId>
    </testCaseLink>
    <testCaseLink>
-      <guid>95ac384f-d74c-417e-bdec-320f4b61bf18</guid>
+      <guid>8d3c123f-372a-48c6-91fa-dd072c9d1b96</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Login Invalid Name and Empty Age</testCaseId>
    </testCaseLink>
    <testCaseLink>
-      <guid>66c9245d-a767-48c5-84eb-3a3acc0c7064</guid>
+      <guid>4da75cfe-872b-4ffa-87b5-3327060abd1f</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Login Successful Case</testCaseId>
